4d8e2ecf3551e5ba5225edbf0a227eb49b370fac
on9jaihk <itz.anson.walker@gmail.com>
2020-05-07 19:20:57 +0800

    Update invite.js

--- cmds/invite.js
+++ cmds/invite.js
@@ -4,10 +4,10 @@ module.exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
     
  let inviteEmbed = new Discord.RichEmbed()
- .setDescription("[**Invite**](http://bit.ly/OdarBot)")
+ .setDescription("[**Invite**](https://discord.com/api/oauth2/authorize?client_id=707568959381045269&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2Finvite%2Fon9jaihk%2Fhelperbot&scope=bot)")
  .setColor("#00ff00")
  .setThumbnail(bicon)
- .addField("Use this invite to invite the bot in your server!", "http://bit.ly/OdarBot")
+ .addField("Use this invite to invite the bot in your server!", "https://discord.com/api/oauth2/authorize?client_id=707568959381045269&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.gg%2Finvite%2Fon9jaihk%2Fhelperbot&scope=bot")
 
  message.channel.send(inviteEmbed);
 
