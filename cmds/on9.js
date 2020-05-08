011bb21d753c86a74c9315f3b88e75de5cd903d4
on9jaihk <64878355+on9jaihk@users.noreply.github.com>
2020-05-08 12:55:08 +0800

    Update on9.js

--- cmds/on9.js
+++ cmds/on9.js
@@ -1,1 +1,18 @@
+const Discord = require("discord.js");
 
+module.exports.run = async (bot, message, args) => {
+    .setThumbnail(bicon)
+    .addField("on9", `區區一個戇鳩仔`, inline)
+    .addField("onl79", "你話咩話?", inline )
+    .addField("on9jai", `洗唔洗加埋 HK 呀? XD`, inline)
+    .addField("onl79hai", `<@616908570411401231> 專用暱稱`, inline)
+    .addField("你再唔交功課", "記你小過XD", inline)
+    .addField("快啲做功課啦", "仲望", inline)
+    
+    message.channel.send(botembed);
+
+}
+
+module.exports.help = {
+  name:"on9"
+}
